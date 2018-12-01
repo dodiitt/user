3b738f7283324c75245bdfa6a1d5c3fc6e2877dd
Jack 'n' Jill <dodhie.virgo4@gmail.com>
2018-12-02 02:48:27 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,10 @@
+<?php
+$msgtoto = "[!] Santai dan Nikmati Hidupmu";
+$msgyd = "[!] Santai dan Nikmati Hidupmu";
+$msgwe = "[!] Santai dan Nikmati Hidupmu";
+ 
+$data= [
+'dodit','dodiitt','dodiit','dodittt','dodiiit','tampan','tuyul','ayah','arsyilla',
+        ];
+$ydnews = ['tes',];
+$toto = ['tes',];
