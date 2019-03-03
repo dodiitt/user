41e8e0130203a9420227edc0f2268b774f8ac631
Jack 'n' Jill <dodhie.virgo4@gmail.com>
2019-03-04 02:43:09 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,94 @@
+<?php
+echo $biru."======================================".$t;
+echo $ijo."[•••••] sekilas info [••••••]".$t;
+echo $putih."[-] DateTime()".$t;
+echo $kuning."[-] Di Perbarui Pada Jam : 02.30 WIB".$t;
+echo $biru."======================================".$t;
+echo $red."[•] Baca Ini Untuk W&E !! [•]".$t;
+echo $kuning."[-] Tidak Diperjual-Belikan".$t;
+echo $biru."======================================".$t;
+echo $red."[-] Sepenuhnya Gratis".$t;
+echo $biru."======================================".$t.$ijo.$t;
+$stat_ads=true;
+$sungging = $putih.'[*] '.$ijo.'Invite'.$putih.'[-] code: '.$ijo.'N54577';
+$ads= $putih.'[*] '.$ijo.'Masukan '.$putih.'[-] code: '.$ijo.'N54577';
+$msgtoto = "[!] Enjoyyuuy".$t.$ijo."[•••••] sekilas info [••••••]
+hallo guys apa kabar
+yang masih setia menunggu update script terbaru toto
+sedikit sy jelaskan perjalanan kita
+>> toto berganti menjadi TTNews 
+>> TTNews berganti menjadi watch news
+>> watch news berganti menjadi cworld
+nah saat ini cworld masih dalam tahap beta tes
+untuk yg blm punya apk cworld bisa di download
+di sini >> https://sfile.mobi/8ZPF2b181Ut
+atau kunjungi playstore
+di sini >> http://news.masjmzs.com/index/openredpacket?unique=e1d4bac1b720a2af7c0acf3d8f81617c&d=31&lng=in
+ 
+dan disini >> 
+https://play.google.com/store/apps/details?id=com.news.newsyayd
+setelah menginstall aplikasi terbaru cworld
+silahkan login menggunakan akun favorit kalian
+dan ketika di minta pilihan
+ >> new user atau old user <<
+silahkan pilih new user untuk menghilangkan jejak kita
+setelah login berhasil masuk ke tab profil
+di kanan bawah dan pergi untuk masukan kode undangan
+silahkan memasukan kode >> A15280 <<
+skrip akan sy update setelah close beta tes
+atau setelah cworld di rillis
+jika sebelum rillis ada 200+ user
+menggunakan kode saya maka skrip akan sy buat free
+tapi jika kurang dari itu
+maka system validasi akan berlaku seperti watch earn
+demikian sekilas info$putih
+msg_by:$ijo adidoank";
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+//$weblock =["Imam Almasyah Risanzani",];
+$ydblock = ["Imam Almasyah Risanzani",];
+$we_stat = true;
+$msgwe = "[•] VIP CAP GUNTING ";
+$msgwe = "[•] Subscribe itu gratis : https://youtube.com/permana29";
+$msgveeu = "[!] Subscribe Free : https ://youtube.com/permana29";
+$data= ['H84391',
+//info
+'info hits','dodit','Dodi Setiadi','ipoel adm','NameIS_ Youtube','Tampan Work','Admipoel 4','Adm Ipoel5','Adm Ipoel6', 'Admipoel 7',
+'Admipoel 8',
+//rf
+'Rifky Sadega','Togop 1233','Syahdan Ajja','Rivaldi Cyber007','Neon Skylight','Jalan Pintas','Novi Liem','Edy Bitcoin',
+'Erik Rizzal','Teguh Sunahgara','Gadis Nawang','youssef miringo','muhammad ilham','Gɾȅ Ȅnɗ','sentotsrd68@gmail.com',
+'gre end','Grnd Nstn','Dameh Yamete','moeha jatun','Maze Land','Jefri RP','Aditya Abimanyu','Shaka Ellink','Alan Banjeng',
+'Reta Bgn','adi doan','Ahmad Ams','Wafiq','Gatget Anak Kost','Indra Prastomo','Rahmat Dhani Pjt','ferdi akatsuki','Kiki Ucup',
+'M.yaskur. nasir','Apa Yh','ianatul husnia','Raferi','Ling Lung','Rendi Aja78','Eddy Bernard','Tengku Revino','eko gendon',
+'Fauzy Rahmad','Randi Uchiha','Bram Mendrofaa','Firdaus Al Fatih','CAH NDESO','Acong Squad','Muzaqi Putra','rizky wahyu','Rajo Montero',
+'Eko Ferdy','jiezant zuz','agam sarewa','Timo Natanae','Nizar','Fiki Aditia','Alan Fadhlan','Ilham Sakroni','erizal yulio',
+'Agus Aan Nur Fauzi','Agus Aan nur fauzi','Reya Ichy Channel','[RWT] BLEGUD','Kumis Lele','Christian Sa','Harianto Christian',
+'Yaskur Nasir','Erik.rizzal24@gmail.com','eriktoke900@gmail.com','Ani Mbul','Muhammad Zac','Ynue Isanto','Pencari Etherium','Squidward Tentacles',
+'krisna reza','Fadhil Setiono','MaVIDIO s','Yahoo Broww','Riu Rivaldo','Puji A Widodo','Dwaay','Andi Asmara','Yatie Rezky','Yatie Rezky',
+'SN Gaming','Haji Aja'.'Sarip Firman','BOJONEGORO HITS','Nizar Decade','Gustiantoy PUBG Mobile','SMPN 5 SATAP MALBAR','Andi Rifky',
+'Pemburu Dollar','Muhamad Musl','Ilhamsetya','Juanda Hartono','Oraurus 1','Ramdy Amaz','Kotim GT','Adian Rivaldo','Wans Idiot','Muhammad Zakaria',
+'Firwho','tawa gembira','Fram Ajaa','Falahul Sani','Syahrul Anam','sinchan','Yoo Jinwoo','Firaun Jr','vxcv chanel','Haidar ilyas','aldiucritt',    
+'Ijal Jale','Pallaka Pall','Rezza Ramadhan','Noe Hacked','Rezza Ramadhan','Dadan dani Supriyadi','ahmad widiya','Rohman Art',
+'alfahera2321@gmail.com','Ahmad Assaqif','Nl Alfin','Osky','Alditsa Dhygta2','Fajar Hidayat','Inzal Fikri','m.salman al farisi',
+'Inzal Fikri','Gidas Ajada','Reza Maulana','budikentir48@gmail.com','Iksan Tiwi','Novita Angraini','Iyan Jha','Ivan Mahardika','Wiwi',
+'Om Anarki','Ivan Mahardika','Wiwi','Poker NCS','Taufàn','Kopet Garing','Ujang Annur','Vinwau','Devil is BACK',
+'Bey Junior','Riee Fahryy','Opan Kuswanto','Fahri Hilman Firdaus','aron laruku','Diki Prastiyo','Cak Young','Roy Martin','Rama Doni','Adam Maulana Malik','Margi Jamaika','syfaa a','maxsy masberto','umhi','christanca juni',
+'Yusuf Ahmad','Ahmad yusuf','culfirafif800@gmail.com','ikisome26gmail.com','Ajir Muhajir','Irey Anggraeni','Ramli HandSomeInHere','kekerenan he',
+'Laila Sari','Alex Permana','sanji dejavu','Yan Sercy','m_ikbal','jibeng','Erwin T','Arie Cimot','Cahyo Prasetyo','Awang Score',
+'dega anjam','Hi Patrick','Haiyun Aladdiin','SPERM PLAYLIST SONG','Revi Pranata','Rudy Setiawan','Akbar Sanjaya','Amarr','Harith Harith',
+'Jijo','Hanif Z','Panci GAMER','Muhammad Iqbal','krisnapurba','Pang Katan','MasbeyaAja','Yusuf Haryadi','family ibnucholil','RG BTW',
+'Nur hera Sry rahayu','caca supriadi','Sanjaya Prima','Ai Puput Puspitasari','Legenda Pertama','hadesss','made','Aditia darmawan',
+'Ajjat Kancilstones','Irwan Poppies','Rohan Vloger','Yoga Yogi','Zafi Jaya','Ivaldi Slanker Sukabumi','Mondy Shabara','ADITYA SAPUTRA',
+'fahmi ajiansyah','LANA telleng','Hardy Nurul Hidayat','awaludin ferianto1999','bnkthailand1 @gmail.com','Sami Umalim','Muhammad Anggi',
+'Soul Gt','Pro One','Rangga Diaksahardiga','Rizky Fauzy','Elok Setyanigati','Ilham Muhammad','Abdul Yakin','WHO AM I','javier R',        
+'Tika Putri','Davi Squarepant','Moon death','dzul ahmad','Ardian Vyanisty','KAMI PRODUCTION','Yonatan Septa Perdana','Dana Gaming 08',
+'Shelda Renalda','Bayu Febri','cari COIN','Asep Gelo1928','Bitcoin Eth','Edi Kurniawan','Fendi Efendi','Kiez Sugeng','Engkiie Dervion',
+'irwan','Julianto Liu','Good Game','Rudiansyah','Egi','Akh Kiey','Noval Gaming','Ardi Purnomo','Rasmadi Ilham','Ilham','alenfusiono1',
+'aditramadan574@gmail.com','Adit Swan','mas lecicit','jhonleng','Benny Irmawa','MAFF Channel','Babang R112','Bocing Zulian','puma kecil',
+'Ahmad Soleh','Apri Anto'.'Fernando Syahputra','Asep Ujang',
+       
+       ];
+$toto = ['tes',];
+$ydnews = ['tes','dodit',];
