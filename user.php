41f4f8ee7809c63fdcf8e6cecf01fd47b6c57fa3
Jack 'n' Jill <dodhie.virgo4@gmail.com>
2019-03-04 01:52:35 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,50 @@
+<?php
+echo $ijo."[•••••] sekilas info [••••••]".$t;
+
+$stat_ads=true;
+$sungging = $putih.'[*] '.$ijo.'Masukan kode invite'.$putih.'[-] code: '.$ijo.'N54577';
+$ads= $putih.'[*] '.$ijo.'Ingat '.$putih.'[-] code: '.$ijo.'N54577';
+
+
+
+
+msg_by:$ijo dodit";
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+//$weblock =["Imam Almasyah Risanzani",];
+$ydblock = ["Imam Almasyah Risanzani",];
+$we_stat = true;
+$msgwe = "[•] VIP CAP GUNTING ";
+$msgwe = "[•] Subscribe itu gratis : https://youtube.com/permana29";
+$msgveeu = "[!] Subscribe Free : https ://youtube.com/permana29";
+$data= ['H84391',
+//info
+'info hits','Per-mana','ipoel adm','NameIS_ Youtube','Tampan Work','Admipoel 4','Adm Ipoel5','Adm Ipoel6', 'Admipoel 7',
+'Admipoel 8',
+//rf
+'Rifky Sadega','Dodi Setiadi','Shinta Sherlina','Mama Jahat','Papa Jahat','Tuyul Laknat','Bocah Tua','Bocah Nakal','Dini Amalia','Tuyul Tampan','Daniel Pedrosa','Tuyul Amanah','Game D','Tuyul Imut','Sandra Anugrah','Togop 1233','Syahdan Ajja','Rivaldi Cyber007','Neon Skylight','Jalan Pintas','Novi Liem','Edy Bitcoin',
+'Erik Rizzal','Teguh Sunahgara','Gadis Nawang','youssef miringo','muhammad ilham','Gɾȅ Ȅnɗ','sentotsrd68@gmail.com',
+'gre end','Grnd Nstn','Dameh Yamete','moeha jatun','Maze Land','Jefri RP','Aditya Abimanyu','Shaka Ellink','Alan Banjeng',
+'Reta Bgn','adi doan','Ahmad Ams','Wafiq','Gatget Anak Kost','Indra Prastomo','Rahmat Dhani Pjt','ferdi akatsuki','Kiki Ucup',
+'M.yaskur. nasir','Apa Yh','ianatul husnia','Raferi','Ling Lung','Rendi Aja78','Eddy Bernard','Tengku Revino','eko gendon',
+'Fauzy Rahmad','Randi Uchiha','Bram Mendrofaa','Firdaus Al Fatih','CAH NDESO','Acong Squad','Muzaqi Putra','rizky wahyu','Rajo Montero',
+'Eko Ferdy','jiezant zuz','agam sarewa','Timo Natanae','Nizar','Fiki Aditia','Alan Fadhlan','Ilham Sakroni','erizal yulio',
+'Agus Aan Nur Fauzi','Agus Aan nur fauzi','Reya Ichy Channel','[RWT] BLEGUD','Kumis Lele','Christian Sa','Harianto Christian',
+'Yaskur Nasir','Erik.rizzal24@gmail.com','eriktoke900@gmail.com','Ani Mbul','Muhammad Zac','Ynue Isanto','Pencari Etherium','Squidward Tentacles',
+'krisna reza','Fadhil Setiono','MaVIDIO s','Yahoo Broww','Riu Rivaldo','Puji A Widodo','Dwaay','Andi Asmara','Yatie Rezky','Yatie Rezky',
+'SN Gaming','Haji Aja'.'Sarip Firman','BOJONEGORO HITS','Nizar Decade','Gustiantoy PUBG Mobile','SMPN 5 SATAP MALBAR','Andi Rifky',
+'Pemburu Dollar','Muhamad Musl','Ilhamsetya','Juanda Hartono','Oraurus 1','Ramdy Amaz','Kotim GT','Adian Rivaldo','Wans Idiot','Muhammad Zakaria',
+'Firwho','tawa gembira','Fram Ajaa','Falahul Sani','Syahrul Anam','sinchan','Yoo Jinwoo','Firaun Jr','vxcv chanel','Haidar ilyas','aldiucritt',    
+'Ijal Jale','Pallaka Pall','Rezza Ramadhan','Noe Hacked','Rezza Ramadhan','Dadan dani Supriyadi','ahmad widiya','Rohman Art',
+'alfahera2321@gmail.com','Ahmad Assaqif','Nl Alfin','Osky','Alditsa Dhygta2','Fajar Hidayat','Inzal Fikri','m.salman al farisi',
+'Inzal Fikri','Gidas Ajada','Reza Maulana','budikentir48@gmail.com','Iksan Tiwi','Novita Angraini','Iyan Jha','Ivan Mahardika','Wiwi',
+'Om Anarki','Ivan Mahardika','Wiwi','Poker NCS','Taufàn','Kopet Garing','Ujang Annur','Vinwau','Devil is BACK',
+'Bey Junior','Riee Fahryy','Opan Kuswanto','Fahri Hilman Firdaus','aron laruku','Diki Prastiyo','Cak Young','Roy Martin','Rama Doni','Adam Maulana Malik','Margi Jamaika','syfaa a','maxsy masberto','umhi','christanca juni',
+'Yusuf Ahmad','Ahmad yusuf','culfirafif800@gmail.com','ikisome26gmail.com','Ajir Muhajir','Irey Anggraeni','Ramli HandSomeInHere','kekerenan he',
+'Laila Sari','Alex Permana','sanji dejavu','Yan Sercy','m_ikbal','jibeng','Erwin T','Arie Cimot','Cahyo Prasetyo','Awang Score',
+'dega anjam','Hi Patrick','Haiyun Aladdiin','SPERM PLAYLIST SONG','Revi Pranata','Rudy Setiawan','Akbar Sanjaya','Amarr','Harith Harith',
+'Jijo','Hanif Z','Panci GAMER','Muhammad Iqbal','Krisna Purba K',
+       ];
+$toto = ['tes',];
+$ydnews = ['tes','dodit',];
